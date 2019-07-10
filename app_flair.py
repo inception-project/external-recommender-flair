abeb03a1d5597d7ae36d300981d41fdb9d638592
bjtulinl <bjtulinl@gmail.com>
2019-07-10 15:54:39 +0200

    #5 - Add production deployment - I set the number of workers and threads of gunicorn as the options. You can run the application like "python3 app_flair.py --workers 2 --threads 2"(default number is 1) - I also attach the json file of the request.

--- app_flair.py
+++ app_flair.py
@@ -6,11 +6,21 @@ from flask import Flask, request, jsonify
 
 from cassis import *
 
-from flair.data import Token,Sentence
+from flair.data import Token, Sentence
 
 from flair.models import SequenceTagger
 
 import argparse
+
+import logging
+
+import gunicorn.app.base
+
+from gunicorn.six import iteritems
+
+import time
+
+from multiprocessing import Lock
 
 # Types
 
@@ -26,15 +36,26 @@ SENTENCE_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence"
 TOKEN_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"
 IS_PREDICTION = "inception_internal_predicted"
 
+# Locks
+lock_ner_train = Lock()
+lock_pos_train = Lock()
+
 # Models
 
 parser = argparse.ArgumentParser(usage="choose ner and pos models", description="help info.")
 
 parser.add_argument("--ner", choices=['ner', 'ner-ontonotes', 'ner-fast', 'ner-ontonotes-fast'],
-                    default="ner",help="choose ner model")
+                    default="ner", help="choose ner model")
 parser.add_argument("--pos", choices=['pos', 'pos-fast'], default="pos", help="choose pos model")
 
+parser.add_argument("--workers", default=1)
+
+parser.add_argument("--threads", default=1)
+
 args = parser.parse_args()
+
+log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_fmt)
 
 tagger: SequenceTagger = SequenceTagger.load(args.ner)
 tagger_pos: SequenceTagger = SequenceTagger.load(args.pos)
@@ -48,8 +69,10 @@ app = Flask(__name__)
 def route_predict_ner():
     json_data = request.get_json()
 
+    logging.warning('ner prediction starts')
     prediction_request = parse_prediction_request(json_data)
     prediction_response = predict_ner(prediction_request)
+    logging.warning('ner prediction ends')
 
     result = jsonify(document=prediction_response.document)
 
@@ -58,16 +81,27 @@ def route_predict_ner():
 
 @app.route("/ner/train", methods=["POST"])
 def route_train_ner():
-    # Return empty response
-    return ('', 204)
+    if (lock_ner_train.acquire(block=False)):
+        # Return empty response
+        try:
+            logging.warning('ner train starts')
+            time.sleep(15)
+            logging.warning('ner train ends')
+        finally:
+            lock_ner_train.release()
+            return ('', 204)
+    else:
+        return ('', 429)
 
 
 @app.route("/pos/predict", methods=["POST"])
 def route_predict_pos():
     json_data = request.get_json()
 
+    logging.warning('pos prediction starts')
     prediction_request = parse_prediction_request(json_data)
     prediction_response = predict_pos(prediction_request)
+    logging.warning('pos prediction ends')
 
     result = jsonify(document=prediction_response.document)
 
@@ -76,8 +110,17 @@ def route_predict_pos():
 
 @app.route("/pos/train", methods=["POST"])
 def route_train_pos():
-    # Return empty response
-    return ('', 204)
+    if (lock_pos_train.acquire(block=False)):
+        # Return empty response
+        try:
+            logging.warning('pos train starts')
+            time.sleep(15)
+            logging.warning('pos train ends')
+        finally:
+            lock_pos_train.release()
+            return ('', 204)
+    else:
+        return ('', 429)
 
 
 def parse_prediction_request(json_object: JsonDict) -> PredictionRequest:
@@ -110,10 +153,10 @@ def predict_ner(prediction_request: PredictionRequest) -> PredictionResponse:
     text = []
     idx = 0
     for sentence in sentences:
-        tokens = [Token(cas.get_covered_text(t)) for t in list(cas.select_covered(TOKEN_TYPE,sentence))]
+        tokens = [Token(cas.get_covered_text(t)) for t in list(cas.select_covered(TOKEN_TYPE, sentence))]
         for token in tokens:
             token.idx = idx
-            idx+=1
+            idx += 1
         s = Sentence()
         s.tokens = tokens
         text.append(s)
@@ -124,7 +167,7 @@ def predict_ner(prediction_request: PredictionRequest) -> PredictionResponse:
         for ent in sen.get_spans('ner'):
             start_idx = ent.tokens[0].idx
 
-            end_idx = start_idx + len(ent.tokens) -1
+            end_idx = start_idx + len(ent.tokens) - 1
             fields = {'begin': tokens_cas[start_idx].begin,
                       'end': tokens_cas[end_idx].end,
                       IS_PREDICTION: True,
@@ -148,10 +191,10 @@ def predict_pos(prediction_request: PredictionRequest) -> PredictionResponse:
     text = []
     idx = 0
     for sentence in sentences:
-        tokens = [Token(cas.get_covered_text(t)) for t in list(cas.select_covered(TOKEN_TYPE,sentence))]
+        tokens = [Token(cas.get_covered_text(t)) for t in list(cas.select_covered(TOKEN_TYPE, sentence))]
         for token in tokens:
             token.idx = idx
-            idx+=1
+            idx += 1
         s = Sentence()
         s.tokens = tokens
         text.append(s)
@@ -174,9 +217,35 @@ def predict_pos(prediction_request: PredictionRequest) -> PredictionResponse:
     return PredictionResponse(xmi)
 
 
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super(StandaloneApplication, self).__init__()
+
+    def load_config(self):
+        config = dict([(key, value) for key, value in iteritems(self.options)
+                       if key in self.cfg.settings and value is not None])
+        for key, value in iteritems(config):
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    ##app.run(debug=True, host='0.0.0.0')
+    # app.run(host='0.0.0.0')
+
+    options = {
+        'bind': '%s:%s' % ('0.0.0.0', '5000'),
+        'workers': args.workers,
+        'threads': args.threads,
+    }
+    StandaloneApplication(app, options).run()
     """
+
     # For debugging purposes, load a json file containing the request and process it.
     import json
     with open("predict.json", "rb") as f:
